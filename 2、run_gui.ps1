6dee806a318b19bcb1448ffa5ef411c69b4da12b
sdbds <865105819@qq.com>
2025-02-08 19:04:26 +0800

    update host args

--- 2、run_gui.ps1
+++ 2、run_gui.ps1
@@ -1,5 +1,6 @@
 # run script by @bdsqlsz
 
+$host_address= "127.0.0.1"
 $port = ""
 $enable_t23d = $false
 $profile_set=3 #5 for highvarm, 4 for high, 3 for normal,2 for low, 1 for ultra
@@ -30,6 +31,10 @@ $Env:TORCH_HOME= $PSScriptRoot+"\torch"
 #$Env:HF_ENDPOINT = "https://hf-mirror.com"
 $Env:XFORMERS_FORCE_DISABLE_TRITON = "1"
 $ext_args = [System.Collections.ArrayList]::new()
+
+if ($host_address) {
+  [void]$ext_args.Add("--host=$host_address")
+}
 
 if ($port) {
   [void]$ext_args.Add("--port=$port")
