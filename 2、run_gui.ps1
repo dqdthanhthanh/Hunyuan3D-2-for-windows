3677e92c3e888ce1b0a6fb6375f29ca8b2749136
sdbds <865105819@qq.com>
2025-03-20 08:51:26 +0800

    update

--- 2、run_gui.ps1
+++ 2、run_gui.ps1
@@ -3,6 +3,14 @@
 $host_address= "127.0.0.1"
 $port = ""
 $enable_t23d = $false
+$disable_tex = $false
+$enable_flashvdm = $false
+$low_vram_mode = $false
+$compile = $true
+$mini = $false
+$turbo = $false
+$mv = $true
+$h2 = $false
 $profile_set=3 #5 for highvarm, 4 for high, 3 for normal,2 for low, 1 for ultra
 
 # Activate python venv
@@ -42,6 +50,38 @@ if ($port) {
 
 if ($enable_t23d) {
   [void]$ext_args.Add("--enable_t23d")
+}
+
+if ($disable_tex) {
+  [void]$ext_args.Add("--disable_tex")
+}
+
+if ($enable_flashvdm) {
+  [void]$ext_args.Add("--enable_flashvdm")
+}
+
+if ($low_vram_mode) {
+  [void]$ext_args.Add("--low-vram-mode")
+}
+
+if ($compile) {
+  [void]$ext_args.Add("--compile")
+}
+
+if ($mini) {
+  [void]$ext_args.Add("--mini")
+}
+
+if ($turbo) {
+  [void]$ext_args.Add("--turbo")
+}
+
+if ($mv) {
+  [void]$ext_args.Add("--mv")
+}
+
+if ($h2) {
+  [void]$ext_args.Add("--h2")
 }
 
 if ($profile_set -ne 3) {
